72143fae9fbd7fb8a5efbd007d26bcdfa3115a9f
nikhil-sharma4040 <nikhilsharma7159@gmail.com>
2024-08-28 15:08:17 +0530

    Create 02.c

---
+++ 02.c
@@ -0,0 +1,13 @@
+/*
+================================================
+Author : NIKHIL SHARMA
+
+Description :
+Write a simple program to execute in an infinite loop at the background. Go to /proc directory and identify all the process related information in the corresponding proc directory.
+================================================
+*/
+
+int main(){
+	while(1);
+	return 0;
+}
