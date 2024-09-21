935cd174161dbb6bbc83ce1514ebd0c93cb9ca1c
nikhil-sharma4040 <nikhilsharma7159@gmail.com>
2024-09-21 15:15:09 +0530

    Create 34b2.c

---
+++ hands_0n_list_2/prog34/34b2.c
@@ -0,0 +1,48 @@
+/*
+NAME:34b2.c
+AUTHOR: NIKHIL SHARMA
+
+PROGRAM:
+34. Write a program to create a concurrent server.
+a. use fork
+b. use pthread_create
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main() {
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("socket");
+        exit(0);
+    }
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8080);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        perror("connect");
+        exit(0);
+    }
+
+    const char* message = "Hello from the client!";
+    send(client_socket, message, strlen(message), 0);
+
+    char buffer[1024];
+    // to avoid garbage values, sending extra data etc
+    memset(buffer, 0, sizeof(buffer));
+    recv(client_socket, buffer, sizeof(buffer), 0);
+    printf("Received message: %s\n", buffer);
+
+    close(client_socket);
+}
+
