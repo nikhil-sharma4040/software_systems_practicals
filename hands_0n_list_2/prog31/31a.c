0cd487f03ad5ebe16dfcc171bd5fc5ffa9e5c21c
nikhil-sharma4040 <nikhilsharma7159@gmail.com>
2024-09-21 15:01:41 +0530

    Create 31a.c

---
+++ hands_0n_list_2/prog31/31a.c
@@ -0,0 +1,40 @@
+/*
+NAME:31a.c
+AUTHOR: NIKHIL SHARMA
+
+PROGRAM:
+31. Write a program to create a semaphore and initialize value to the semaphore.
+a. create a binary semaphore
+b. create a counting semaphore
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/sem.h>
+union semun {
+	int val;
+	struct semid_ds *buf;
+	unsigned short int *array;
+};
+int main() {
+
+	key_t key = ftok(".", 1);
+	if( key == -1){ perror("ftok");exit(0);}
+
+	int semid = semget(key, 1, IPC_CREAT|0777);//no of semaphores = 1
+
+        union semun arg;
+	arg.val = 1;//binary semaphore
+
+	int ret = semctl(semid,0,SETVAL,arg);
+	if (ret != -1){
+    		printf("Binary semaphore created and initialized\n");
+	}
+	else{
+		perror("semctl");
+		exit(0);
+	}
+}
+
