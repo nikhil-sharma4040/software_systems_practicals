d5d939c592987f897b7c1598a32c418cd15533a7
nikhil-sharma4040 <nikhilsharma7159@gmail.com>
2024-09-21 15:00:42 +0530

    Create sample_output_30c.c

---
+++ hands_0n_list_2/prog30/sample_output_30c.c
@@ -0,0 +1,19 @@
+aditya@laptop:~/SS-Lab/SS-Hands-on-List-2/pg30$ ./30c.out
+shared memory id : 32816
+Data stored is hello
+Shared memory detach successful
+
+------ Shared Memory Segments --------
+key        shmid      owner      perms      bytes      nattch     status      
+0x00000000 5          aditya     600        524288     2          dest         
+0x00000000 32778      aditya     600        524288     2          dest         
+0x00000000 11         aditya     600        524288     2          dest         
+0x00000000 32780      aditya     600        4194304    2          dest         
+0x00000000 32781      aditya     600        67108864   2          dest         
+0x00000000 32784      aditya     600        524288     2          dest         
+0x00000000 32785      aditya     600        4194304    2          dest         
+0x00000000 32786      aditya     600        33554432   2          dest         
+0x00000000 32807      aditya     600        524288     2          dest         
+0xffffffff 32815      aditya     744        1024       0                       
+0x0106b12f 32816      aditya     744        1024       0                       
+0x00000000 60         aditya     600        524288     2          dest       
